2dc18b7b65570555e5429094751e37cfec6bfae3
Lucas Åström <lucasas@kth.se>
2016-08-09 17:43:59 +0200

    whoops, uncomment stuff

--- download_data.py
+++ download_data.py
@@ -4,34 +4,34 @@ import subprocess
 url = "http://vision.in.tum.de/rgbd/dataset/%s/rgbd_dataset_%s_%s.tgz"
 
 names = ["freiburg3_sitting_static",
-         "freiburg3_walking_static"]
-         #"freiburg1_xyz",
-         #"freiburg1_rpy",
-         #"freiburg2_xyz",
-         #"freiburg2_rpy",
-         #"freiburg1_360",
-         #"freiburg1_floor",
-         #"freiburg1_desk",
-         #"freiburg1_desk2",
-         #"freiburg1_room",
-         #"freiburg2_360_hemisphere",
-         #"freiburg2_desk",
-         #"freiburg2_large_no_loop",
-         #"freiburg2_large_with_loop",
-         #"freiburg3_long_office_household",
-         #"freiburg2_pioneer_slam",
-         #"freiburg3_nostructure_notexture_far",
-         #"freiburg3_nostructure_notexture_near_withloop",
-         #"freiburg3_nostructure_texture_far",
-         #"freiburg3_nostructure_texture_near_withloop",
-         #"freiburg3_structure_notexture_far",
-         #"freiburg3_structure_notexture_near",
-         #"freiburg3_structure_texture_far",
-         #"freiburg3_structure_texture_near",
-         #"freiburg2_desk_with_person",
-         #"freiburg3_sitting_static",
-         #"freiburg3_sitting_halfsphere",
-         #"freiburg3_walking_halfsphere"]
+         "freiburg3_walking_static",
+         "freiburg1_xyz",
+         "freiburg1_rpy",
+         "freiburg2_xyz",
+         "freiburg2_rpy",
+         "freiburg1_360",
+         "freiburg1_floor",
+         "freiburg1_desk",
+         "freiburg1_desk2",
+         "freiburg1_room",
+         "freiburg2_360_hemisphere",
+         "freiburg2_desk",
+         "freiburg2_large_no_loop",
+         "freiburg2_large_with_loop",
+         "freiburg3_long_office_household",
+         "freiburg2_pioneer_slam",
+         "freiburg3_nostructure_notexture_far",
+         "freiburg3_nostructure_notexture_near_withloop",
+         "freiburg3_nostructure_texture_far",
+         "freiburg3_nostructure_texture_near_withloop",
+         "freiburg3_structure_notexture_far",
+         "freiburg3_structure_notexture_near",
+         "freiburg3_structure_texture_far",
+         "freiburg3_structure_texture_near",
+         "freiburg2_desk_with_person",
+         "freiburg3_sitting_static",
+         "freiburg3_sitting_halfsphere",
+         "freiburg3_walking_halfsphere"]
 
 def get_url(name):
     folder = name[:9]
@@ -49,7 +49,7 @@ except:
 os.chdir("data/TUM")
 
 for name in names:
-    #os.system("wget " + get_url(name) + " -O " + name + ".tgz")
+    os.system("wget " + get_url(name) + " -O " + name + ".tgz")
     full_name = "rgbd_dataset_" + name
     os.system("tar -xvf " + name + ".tgz")
     os.system("rm -rf " + name + ".tgz")
