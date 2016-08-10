30b73f047000b104ca6599bc1df6f346e35cd6e2
Lucas Åström <lucasas@kth.se>
2016-08-10 09:59:47 +0200

    eval script doesn't need build 'script'

--- eval.sh
+++ eval.sh
@@ -1,4 +1,4 @@
 for var in "$@"
 do
-    ./build.sh && ./lsdslam.out "$var" loop && python data/evaluate_rpe.py data/TUM/$var/groundtruth.txt output/"$var"_loop.poses --verbose --fixed_delta --delta_unit f --plot output/"$var"_loop.png > output/"$var"_loop.result
+    make -j4 && ./lsdslam.out "$var" loop && python data/evaluate_rpe.py data/TUM/$var/groundtruth.txt output/"$var"_loop.poses --verbose --fixed_delta --delta_unit f --plot output/"$var"_loop.png > output/"$var"_loop.result
 done
